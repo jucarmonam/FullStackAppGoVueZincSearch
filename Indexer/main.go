77d39327851233a72813042e8e498b34a6bde5e2
Juan Pablo <jucarmonam@unal.edu.co>
2023-07-25 12:44:50 -0500

    finished by taking so much time

--- Indexer/main.go
+++ Indexer/main.go
@@ -3,34 +3,66 @@ package main
 import (
 	"Indexer/models"
 	"bufio"
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"path/filepath"
+	"runtime"
+	"runtime/pprof"
 	"strings"
 )
 
 func main() {
-	//read fyle system folder
-	//var finalEmails []string
-	path := "C:\\Users\\juan\\Desktop\\Prueba truora\\enron_mail_20110402\\maildir"
+	path := "C:\\Users\\juan\\Desktop\\PruebaTruora\\enron_mail_20110402\\maildir"
+	//path := os.Args[1]
+
+	////Proceso de rendimiento de la aplicación/////////////
+	cpu, err := os.Create("cpu.prof")
+	if err != nil {
+		log.Fatal(err)
+	}
+	pprof.StartCPUProfile(cpu)
+	defer pprof.StopCPUProfile()
+	////////Fin prceso de rendimiento de la aplicación/////////
 
 	fmt.Println("Indexing")
-
+	//read fyle system folder
 	allEmails := readFolder(path)
-	jsonData, err := json.Marshal(allEmails)
 
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
+	var ndjson bytes.Buffer
+
+	// Generate the NDJSON variable
+	for _, email := range allEmails {
+		jsonData, err := json.Marshal(email)
+		if err != nil {
+			fmt.Println("Error marshaling JSON:", err)
+			return
+		}
+		ndjson.Write(jsonData)
+		ndjson.WriteString("\n")
 	}
 
-	fmt.Println(string(jsonData))
+	//Inserting in zinsearch
+	zincSearchInsert(ndjson.String())
+
+	////Proceso de rendimiento de la aplicación/////////////
+	runtime.GC()
+	mem, err := os.Create("memory.prof")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer mem.Close()
+	if err := pprof.WriteHeapProfile(mem); err != nil {
+		log.Fatal(err)
+	}
+	////Fin proceso de rendimiento de la aplicación/////////////
 }
 
 func readFolder(mainPath string) []models.Email {
-	var folderList []string
 	emails := make([]models.Email, 0)
 	counter := 0
 	err := filepath.Walk(mainPath, func(path string, info os.FileInfo, err error) error {
@@ -39,14 +71,8 @@ func readFolder(mainPath string) []models.Email {
 			return err
 		}
 
-		// Calculate the depth of the current directory
-		depth := strings.Count(path, string(os.PathSeparator)) - strings.Count(mainPath, string(os.PathSeparator))
-		if depth == 1 {
-			folderList = append(folderList, info.Name())
-		}
-
-		if counter == 1 {
-			return filepath.SkipDir // Exit the loop
+		if counter == 1000 {
+			return filepath.SkipDir
 		}
 
 		if info.IsDir() {
@@ -74,14 +100,18 @@ func readFile(name string) models.Email {
 	}
 	defer file.Close()
 
+	buf := []byte{}
 	scanner := bufio.NewScanner(file)
+	scanner.Buffer(buf, 2048*1024)
 	newEmail := models.Email{}
 
+	lineNumber := 0
+	counter := 0
 	for scanner.Scan() {
+		lineNumber++
 		line := scanner.Text()
-
 		index := strings.Index(line, ":")
-		if index != -1 && index < len(line)-1 {
+		if index != -1 && index < len(line)-1 && counter < 5 {
 			key := strings.TrimSpace(line[:index])
 			value := strings.TrimSpace(line[index+1:])
 
@@ -98,14 +128,37 @@ func readFile(name string) models.Email {
 			case "Subject":
 				newEmail.Subject = value
 			}
+			counter++
 		} else {
 			newEmail.Body = newEmail.Body + line + "\n"
 		}
 	}
 
 	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+		log.Fatalf("something bad happened in the line %v: %v", lineNumber, err)
 	}
 
 	return newEmail
+}
+
+func zincSearchInsert(emails string) {
+	req, err := http.NewRequest("POST", "http://localhost:4080/api/emails/_multi", strings.NewReader(emails))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.SetBasicAuth("admin", "Complexpass#123")
+	req.Header.Set("Content-Type", "application/json")
+	//req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	log.Println(resp.StatusCode)
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(body))
 }
